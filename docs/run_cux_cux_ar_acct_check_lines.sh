a22a6669235747dbfc40645bfe0f5b910e9448f3
tony <tony1@kinwong.com>
2025-07-31 09:54:03 +0800

    feat:执行本地shell脚本

--- docs/run_cux_cux_ar_acct_check_lines.sh
+++ docs/run_cux_cux_ar_acct_check_lines.sh
@@ -14,8 +14,20 @@ BIZ_DATE=`date +%Y%m%d -d "${DATA_DATE} -1 day"`
 echo "脚本执行日期:${DATA_DATE}, 业务数据计算日期:${BIZ_DATE}"
 echo ">>>数据同步开始"
 spark-sql --master yarn --queue root --deploy-mode client --conf spark.debug.maxToStringFields=300 --conf spark.executor.instances=4 --conf spark.executor.memory=5g --conf spark.executor.cores=3 -e"
+create table if not exists bi_tmp.ods_cux_cux_ar_acct_check_lines
+USING org.apache.spark.sql.jdbc
+OPTIONS (
+    url "jdbc:oracle:thin:@ebsdb-scan.kinwong.com:1531/prod",
+    dbtable "CUX.CUX_AR_ACCT_CHECK_LINES",
+    driver "oracle.jdbc.driver.OracleDriver",
+    user "hadoop",
+    password "vSWnGLcdd8ch"
+);
+
+select * from bi_tmp.ods_cux_cux_ar_acct_check_lines limit 100;
+
 insert overwrite table bi_data.dwd_oe_order_ds2 partition(pt_d)
-select * from bi_data.dwd_oe_order_ds
+select * from bi_data.dwd_oe_order_ds;
 "
 if [[ $? -eq 0 ]]; then
 	echo ">>>数据同步结束，同步日期:${BIZ_DATE}"
